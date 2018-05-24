4bc3d44aad679824e846449901d92ffcfa84e5b4
James Reed <jamesreed@fb.com>
2018-05-24 13:01:10 -0700

    script version

--- word_language_model/model.py
+++ word_language_model/model.py
@@ -1,5 +1,64 @@
 import torch.nn as nn
 from torch.autograd import Variable
+import torch
+from torch.nn import functional as F
+
+@torch.jit.script
+def lstm_cell(input_, hx, cx, w_hh, b_hh):
+    gates = input_.squeeze(0) + torch.addmm(b_hh, hx, w_hh.t())
+
+    ingate, forgetgate, cellgate, outgate = gates.chunk(4, 1)
+
+    ingate = F.sigmoid(ingate)
+    forgetgate = F.sigmoid(forgetgate)
+    cellgate = F.tanh(cellgate)
+    outgate = F.sigmoid(outgate)
+    cy = (forgetgate * cx) + (ingate * cellgate)
+    hy = outgate * F.tanh(cy)
+
+    return hy, cy
+
+BLOCK_SIZE = 8
+
+@torch.jit.script
+def lstm_block(input_, hx, cx, w_hh, b_hh):
+    outputs = 0
+    for i in range(8):
+        input_t = torch.index_select(input_, dim=0, index=i)
+        hx, cx = lstm_cell(input_t, hx, cx, w_hh, b_hh)
+        outputs
+        if i == 0:
+            outputs = hx.unsqueeze(0)
+        else:
+            outputs = torch.cat((outputs, hx.unsqueeze(0)), dim=0)
+    return outputs, cx
+
+print(lstm_block.graph)
+
+def lstm(input, hidden, w_ih, w_hh, b_ih, b_hh):
+    hx, cx = hidden[0][0], hidden[1][0]
+    seq_len, batch_size, input_size = input.size()
+    input_ = F.linear(input.view(-1, input_size), w_ih, b_ih).view(seq_len, batch_size, -1)
+    output = 1
+    for i in range(0, input.size(0), BLOCK_SIZE):
+        if i + BLOCK_SIZE <= input.size(0):
+            o, cx = lstm_block(input_.narrow(0, i, 8), hx, cx, w_hh, b_hh)
+            hx = o[-1]
+            if i == 0:
+                output = o
+            else:
+                output = torch.cat((output, o), dim=0)
+        else:
+            for ii in range(i, min(i + BLOCK_SIZE, input.size(0))):
+                hx, cx = lstm_cell(input_[ii], hx, cx, w_hh, b_hh)
+                if ii == 0:
+                    output = hx.unsqueeze(0)
+                else:
+                    output = torch.cat((output, hx.unsqueeze(0)), dim=0)
+
+    #print(lstm_cell.jit_debug_info())
+    return output, (hx.view(1, *hx.size()), cx.view(1, *cx.size()))
+
 
 class RNNModel(nn.Module):
     """Container module with an encoder, a recurrent module, and a decoder."""
@@ -45,7 +104,8 @@ class RNNModel(nn.Module):
     def forward(self, input, hidden):
         emb = self.drop(self.encoder(input))
         self.rnn.flatten_parameters()
-        output, hidden = self.rnn(emb, hidden)
+        # output, hidden = self.rnn(emb, hidden)
+        output, hidden = lstm(emb, hidden, *self.rnn.all_weights[0])
         output = self.drop(output)
         decoded = self.decoder(output.view(output.size(0)*output.size(1), output.size(2)))
         return decoded.view(output.size(0), output.size(1), decoded.size(1)), hidden
